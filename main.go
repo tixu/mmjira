909278f3de69b33cc33f18a2c0f943f938f84798
xavier zebier <xavier.ernest.zebier@gmail.com>
2016-06-22 08:24:37 +0200

    creation of struct jira representing our server

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"expvar"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -12,46 +11,35 @@ import (
 
 	"net/http"
 	"strconv"
-	"time"
 
-	metrics "github.com/armon/go-metrics"
 	"github.com/go-yaml/yaml"
 	"github.com/gorilla/mux"
 )
 
 // Config is shared accross all the application
-var (
-	Config        InstanceConfig
-	hitsperminute = expvar.NewInt("hits_per_minute")
-	inm           = metrics.NewInmemSink(10*time.Millisecond, 50*time.Millisecond)
-)
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func (b MMJira) homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>MatterBridge Handler</h1><div>Go bridge</div>")
 }
 
-func getHandler(w http.ResponseWriter, r *http.Request) {
+func (b MMJira) getHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "<h1>MatterBridge Handler</h1><div>Go bridge : get handler</div>")
-	data := inm.Data()
-	intvM := data[0]
-	intvM.RLock()
-	log.Printf("%+v", inm)
-	intvM.RUnlock()
 }
 
-//---------------------------------------------------------------------------------
-
-var mmpost *mmcontroller.MMController
+// MMJira is the heart of the bots
+type MMJira struct {
+	c *InstanceConfig
+	m *mmcontroller.MMController
+}
 
 // GetTarget retrieve the hook assigned to a projet, return an error in anyother case
 
-func postHandler(w http.ResponseWriter, r *http.Request) {
+func (b MMJira) postHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("received a request")
 
-	inm.IncrCounter([]string{"request", "jira"}, 1)
-	if Config.Debug {
-		if err := utils.DumpRequest(r, Config.DumpDir); err != nil {
+	if b.c.Debug {
+		if err := utils.DumpRequest(r, b.c.DumpDir); err != nil {
 			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 		}
 	}
@@ -69,8 +57,9 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	// We only know our top-level keys are strings
 
 	log.Printf("%+v", issue)
-	go mmpost.Analyse(mmpost.Inform(issue))
 
+	ch := b.m.Inform(issue)
+	go b.m.Analyse(ch)
 }
 
 func main() {
@@ -80,20 +69,24 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-
-	if err = yaml.Unmarshal(data, &Config); err != nil {
+	var config InstanceConfig
+	if err = yaml.Unmarshal(data, &config); err != nil {
 		log.Panic(err)
 	}
-	mmpost, err = mmcontroller.NewController(Config.MMicon, Config.MMuser, Config.Hooks)
+	b := MMJira{c: &config}
+
+	mmpost, err := mmcontroller.NewController(b.c.MMicon, b.c.MMuser, b.c.Hooks)
 	if err != nil {
 		panic(err)
 	}
 
-	log.Printf("config : %+v", Config)
-	r.HandleFunc("/", homeHandler)
-	r.HandleFunc("/hooks/", getHandler).Methods("GET")
-	r.HandleFunc("/hooks/", postHandler).Methods("POST")
+	b.m = mmpost
+	log.Printf("config : %+v", b.c)
+	r.HandleFunc("/", b.homeHandler)
+
+	r.HandleFunc("/hooks/", b.getHandler).Methods("GET")
+	r.HandleFunc("/hooks/", b.postHandler).Methods("POST")
 	http.Handle("/", r)
-	endpoint := Config.Host + ":" + strconv.Itoa(Config.Port)
+	endpoint := b.c.Host + ":" + strconv.Itoa(b.c.Port)
 	log.Fatal(http.ListenAndServe(endpoint, r))
 }
