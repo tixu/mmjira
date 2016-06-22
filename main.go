9ff658385803cccb4422dd2b5dfb5756dedfe890
xavier zebier <xavier.ernest.zebier@gmail.com>
2016-06-22 21:56:50 +0200

    various enhancement

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
+	"github.com/uber-go/zap"
 
 	"github.com/tixu/mmjira/jira"
 	"github.com/tixu/mmjira/mmcontroller"
@@ -16,31 +16,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Config is shared accross all the application
+
+
+
+// MMJira is the heart of the bots
+type MMJira struct {
+	c *InstanceConfig
+	m *mmcontroller.MMController
+	r *mux.Router
+	l zap.Logger
+}
 
 func (b MMJira) homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>MatterBridge Handler</h1><div>Go bridge</div>")
 }
 
 func (b MMJira) getHandler(w http.ResponseWriter, r *http.Request) {
-
+	b.l.Debug("got a request to the hetHandler")
 	fmt.Fprintf(w, "<h1>MatterBridge Handler</h1><div>Go bridge : get handler</div>")
 }
 
-// MMJira is the heart of the bots
-type MMJira struct {
-	c *InstanceConfig
-	m *mmcontroller.MMController
-}
 
 // GetTarget retrieve the hook assigned to a projet, return an error in anyother case
-
 func (b MMJira) postHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("received a request")
+	b.l.Debug("received a request")
 
 	if b.c.Debug {
 		if err := utils.DumpRequest(r, b.c.DumpDir); err != nil {
-			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			b.l.Info("unable to dump the request in the directory",zap.String("Directory",b.c.DumpDir))
+
 		}
 	}
 	issue, err := jira.New(r.Body)
@@ -56,24 +60,30 @@ func (b MMJira) postHandler(w http.ResponseWriter, r *http.Request) {
 
 	// We only know our top-level keys are strings
 
-	log.Printf("%+v", issue)
+	b.l.Debug("sending",zap.Object("issue", issue))
 
 	ch := b.m.Inform(issue)
 	go b.m.Analyse(ch)
 }
 
-func main() {
+func (b MMJira) start(){
+	http.Handle("/", b.r)
 
-	r := mux.NewRouter()
+	endpoint := b.c.Host + ":" + strconv.Itoa(b.c.Port)
+	b.l.Fatal("error server",zap.Error(http.ListenAndServe(endpoint, b.r)))
+}
+
+func newBot() (b MMJira){
+  b = MMJira{l: zap.NewJSON(zap.DebugLevel)}
 	data, err := ioutil.ReadFile("config.yaml")
 	if err != nil {
-		log.Panic(err)
+		b.l.Panic("not able to read the file",zap.Error(err))
 	}
 	var config InstanceConfig
 	if err = yaml.Unmarshal(data, &config); err != nil {
-		log.Panic(err)
+		b.l.Panic("not able to marshal the file",zap.Error(err))
 	}
-	b := MMJira{c: &config}
+	b.c= &config
 
 	mmpost, err := mmcontroller.NewController(b.c.MMicon, b.c.MMuser, b.c.Hooks)
 	if err != nil {
@@ -81,12 +91,18 @@ func main() {
 	}
 
 	b.m = mmpost
-	log.Printf("config : %+v", b.c)
-	r.HandleFunc("/", b.homeHandler)
+	b.l.Debug("outputting config", zap.Object("config", b.c))
 
-	r.HandleFunc("/hooks/", b.getHandler).Methods("GET")
-	r.HandleFunc("/hooks/", b.postHandler).Methods("POST")
-	http.Handle("/", r)
-	endpoint := b.c.Host + ":" + strconv.Itoa(b.c.Port)
-	log.Fatal(http.ListenAndServe(endpoint, r))
+	b.r = mux.NewRouter()
+	b.r.HandleFunc("/", b.homeHandler)
+
+	b.r.HandleFunc("/hooks/", b.getHandler).Methods("GET")
+	b.r.HandleFunc("/hooks/", b.postHandler).Methods("POST")
+  return b
+
+}
+
+func main() {
+	b:= newBot()
+	b.start()
 }
